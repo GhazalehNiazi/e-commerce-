f47622a92007cc21571805feaac6440528545bfe
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-04-18 21:15:54 +0430

    changed register page styles

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import LandingPage from "./components/landingPage/LandingPage";
 import MainShoppingPage from "./components/shopping/MainShoppingPage";
 import Order from "./components/order/Order";
 import Cart from "./components/cart/Cart";
+import { Routes, Route, Link } from "react-router-dom";
 
 // sass --watch src/components/styles/sass:src/components/styles/css
 function App() {
   return (
     <div className="">
-      {/* <Register/>*/}
-      {/*<LandingPage/> */}
-    { /* <MainShoppingPage/>*/}
-  {/* <Order/> */}  
-  <Cart/>
+    <Routes>
+    <Route path='/' element={<LandingPage/>}/>
+    <Route path='register' element={<Register/>}/>
+    <Route path='shopping' element={<MainShoppingPage/>}/>
+    <Route path='order' element={<Order/>}/>
+    <Route path='cart' element={<Cart/>}/>
+    </Routes>
     </div>
   );
 }
--- src/components/register/SignInForm.js
+++ src/components/register/SignInForm.js
@@ -17,12 +17,12 @@ function SignInForm(props) {
         <button type="submit" className="button">
           Log in
         </button>
+        <div>
+          <a href="#" className="login">
+            Forgot Password?
+          </a>
+        </div>
       </form>
-      <div>
-        <a href="#" className="login">
-          Forgot Password?
-        </a>
-      </div>
       <div className="changeMethod">
         <p>Dont't have an account?</p>
         <a  className="login" onClick={props.onChangeMethod}>
